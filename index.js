f138558f56d6e26ec324c6f91cec7296c0704342
youssefmahersi <38184710+youssefmahersi@users.noreply.github.com>
2022-07-11 23:51:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const testFolder = './solidity2/';
+const fs = require('fs');
+var path = require('path')
+
+fs.readdirSync(testFolder).forEach(folder =>{
+    if(fs.lstatSync(testFolder+folder).isFile() && path.extname(folder)!== ".mp4"){
+        fs.unlink(testFolder+folder, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                
+                    console.log(folder+" deleted successfully !");
+                
+        }) 
+    }else if (fs.lstatSync(testFolder+folder).isDirectory()){
+        fs.readdirSync(testFolder+folder).forEach(file => {
+            if(path.extname(file)!== ".mp4"){
+                fs.unlink(testFolder+folder+"/"+file, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                
+                    console.log(file +" deleted successfully !");
+                
+                })
+            }
+            
+          }); 
+    }
+});
+
